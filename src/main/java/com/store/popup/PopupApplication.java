04fc9159e8aae24b210a24b8ee07517ca9e4bccb
milcho0604 <milcho0604@gmail.com>
2025-09-21 20:37:54 +0900

    init

---
+++ src/main/java/com/store/popup/PopupApplication.java
@@ -0,0 +1,13 @@
+package com.store.popup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PopupApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PopupApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/store/popup/PopupApplicationTests.java
@@ -0,0 +1,13 @@
+package com.store.popup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PopupApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
